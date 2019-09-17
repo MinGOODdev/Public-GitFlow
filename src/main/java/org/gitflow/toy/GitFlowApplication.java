c6f626063c29020eb097f469b62dd77dad35c068
조민국(GROOT)/Music사업TF/SKP <skp.9261059@sk.com>
2019-09-18 00:12:03 +0900

    Project Init

---
+++ src/main/java/org/gitflow/toy/GitFlowApplication.java
@@ -0,0 +1,13 @@
+package org.gitflow.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFlowApplication.class, args);
+    }
+
+}
